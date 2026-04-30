6183ef1fdde3103f3ea2445064447f92a3799697
nishthamendpara <nishthamendpara@gmail.com>
2026-04-30 09:35:30 +0530

    Create Pr19.java

---
+++ Pr19.java
@@ -0,0 +1,47 @@
+package student;
+
+public class Student {
+    protected int rollNo;
+    protected String name;
+
+    public Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public void displayStudent() {
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Name: " + name);
+    }
+}
+package exam;
+
+import student.Student;
+
+public class Result extends Student {
+    int marks1, marks2, marks3;
+
+    public Result(int rollNo, String name, int m1, int m2, int m3) {
+        super(rollNo, name);
+        marks1 = m1;
+        marks2 = m2;
+        marks3 = m3;
+    }
+
+    public void displayResult() {
+        displayStudent();
+        int total = marks1 + marks2 + marks3;
+        double average = total / 3.0;
+
+        System.out.println("Marks1: " + marks1);
+        System.out.println("Marks2: " + marks2);
+        System.out.println("Marks3: " + marks3);
+        System.out.println("Total: " + total);
+        System.out.println("Average: " + average);
+    }
+
+    public static void main(String[] args) {
+        Result r = new Result(101, "Rahul", 80, 75, 90);
+        r.displayResult();
+    }
+}
